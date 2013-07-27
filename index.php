c9d3d84bb48052c6a11fe434eb35012060dd57c8
Pedro <Ped@Pedros-MacBook-Air.local>
2013-07-27 20:23:41 +0100

    stuff

--- index.php
+++ index.php
@@ -12,114 +12,33 @@
     <div id="main-content" class="container">
 
         <div class="row">
-            <div class="span12">
-                <div class="hero-unit well">
-                    <h1>Easy WordPress Development BLOG</h1>
-                    <p>This template was developed for a tutorial series about WordPress development. You can find the series on <a href="http://www.youtube.com/easydevtuts" target="_blank">YouTube</a></p>
-                    <p><a href="http://www.easydevtuts.com" target="_blank" class="btn btn-inverse btn-large">Learn more »</a></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="span4">
-                <ul class="nav nav-pills">
-                    <li class="active"><a href="#tab-one" data-toggle="tab">Tutorials</a></li>
-                    <li><a href="#tab-two" data-toggle="tab">Blog</a></li>
-                    <li><a href="#tab-three" data-toggle="tab">Categories</a></li>
-                </ul>
-                <div id="home-tab-container" class="tab-content well">
-                    <div class="tab-pane fade active in" id="tab-one">
-                        <h3>List of Tutorials</h3>
-                        <ol>
-                            <li><a href="#">Tutorials Post</a></li>
-                            <li><a href="#">Tutorials Post</a></li>
-                            <li><a href="#">Tutorials Post</a></li>
-                            <li><a href="#">Tutorials Post</a></li>
-                            <li><a href="#">Tutorials Post</a></li>
-                        </ol>
-                    </div>
-                    <div class="tab-pane fade" id="tab-two">
-                        <h3>List of Blog Postings</h3>
-                        <ol>
-                            <li><a href="#">Recent Post</a></li>
-                            <li><a href="#">Recent Post</a></li>
-                            <li><a href="#">Recent Post</a></li>
-                            <li><a href="#">Recent Post</a></li>
-                            <li><a href="#">Recent Post</a></li>
-                        </ol>
-                    </div>
-                    <div class="tab-pane fade" id="tab-three">
-                        <h3>List of Categories</h3>
-                        <ol>
-                            <li><a href="#">Recent Post</a></li>
-                            <li><a href="#">Recent Post</a></li>
-                            <li><a href="#">Recent Post</a></li>
-                            <li><a href="#">Recent Post</a></li>
-                            <li><a href="#">Recent Post</a></li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-
             <div class="span8">
-                <div id="slider" class="carousel slide hidden-phone">
-                    <div class="carousel-inner">
-                        <div class="item active">
-                            <img src="http://placehold.it/800x400&text=easy" alt="#">
-                            <div class="carousel-caption  hidden-phone">
-                                <h4>First Slide Title</h4>
-                                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img src="http://placehold.it/800x400&text=dev" alt="#">
-                            <div class="carousel-caption hidden-phone">
-                                <h4>Second Slide Title</h4>
-                                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img src="http://placehold.it/800x400&text=tuts" alt="#">
-                            <div class="carousel-caption hidden-phone">
-                                <h4>Third Slide Title</h4>
-                                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            </div>
-                        </div>
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+                    <div class="media">
+                      <a class="pull-left" href="#">
+                        <?php the_post_thumbnail('thumbnail'); ?>
+                      </a>
+                      <div class="media-body">
+                        <h4 class="media-heading"><?php the_title(); ?></h4>
+
+                            <?php the_content(); ?>
+
+                            <a href="<?php the_permalink(); ?>" class="btn btn-mini btn-info">read more</a>
+                      </div>
                     </div>
-                    <ol class="carousel-indicators">
-                        <li data-target="#slider" data-slide-to="0" class="active"></li>
-                        <li data-target="#slider" data-slide-to="1"></li>
-                        <li data-target="#slider" data-slide-to="2"></li>
-                    </ol>
-                </div>
+                <!-- post -->
+                <?php endwhile; ?>
+                <!-- post navigation -->
+                <?php else: ?>
+                <!-- no posts found -->
+                <?php endif; ?>
+            </div>
+            <div class="span4">
+                <?php get_sidebar(); ?>
             </div>
         </div>
 
-        <div class="row">
-            <div class="span12">
-                <ul class="thumbnails">
-                    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                       <!-- post -->
-                            <li class="span4">
-                                <div class="thumbnail">
-                                    <?php the_post_thumbnail(); ?>
-                                    <div class="caption">
-                                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                                            <?php the_excerpt(); ?>
-                                        <p><a href="<?php the_permalink(); ?>" class="btn btn-mini">read more</a></p>
-                                    </div>
-                                </div>
-                            </li>
-
-                       <?php endwhile; ?>
-                       <!-- post navigation -->
-                       <?php else: ?>
-                       <!-- no posts found -->
-                       <?php endif; ?>   
-                </ul>
-            </div>
-        </div>
     </div>
     <!-- end of main home content -->
 
